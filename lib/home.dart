06d27196d4b86b484cdaf009dddc08270c87ebf3
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-02 03:31:05 +0530

    Update Home
    - Updated the Home widget to return the appropriate root widget based on the current menu based on the provider state

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,9 @@ import 'package:provider/provider.dart';
 
 import 'package:terrascope_app/appbar/menustate.dart';
 import 'package:terrascope_app/appbar/custombar.dart';
+import 'package:terrascope_app/regions/regionspage.dart';
+import 'package:terrascope_app/geogallery/geogallerypage.dart';
+import 'package:terrascope_app/userlogin/userpage.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -24,25 +27,18 @@ class Home extends StatelessWidget {
         children: [
           CustomBar(),
           Expanded(
-            child: Consumer<MenuState>(
-              builder: (context, value, child) {
-                if (value.currentmenu == MenuType.userlogin)
-                  return Container(
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                  );
-                else
-                  return Container(
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.white,
-                    ),
-                  );
-              },
+            child: Container(
+              margin: EdgeInsets.all(50),
+              child: Consumer<MenuState>(
+                builder: (context, value, child) {
+                  if (value.currentmenu == MenuType.geogallery)
+                    return GeoGalleryPage();
+                  else if (value.currentmenu == MenuType.regions)
+                    return RegionsPage();
+                  else
+                    return UserPage();
+                },
+              ),
             ),
           ),
         ],
