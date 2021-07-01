1f9ee96903d18dea6aa7f66b4bd0cdac647233de
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-02 00:39:29 +0530

    Update appbar
    - Changed the structure of the NavBar widget
    - Changed the wideness breakpoints
    - Added context coloring scheme

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:mdi/mdi.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomAppBar extends StatelessWidget {
@@ -13,8 +16,6 @@ class CustomAppBar extends StatelessWidget {
         children: [
           LogoBox(),
           Spacer(),
-          UserBox(),
-          SizedBox(width: 15),
           NavBar(),
         ],
       ),
@@ -42,15 +43,16 @@ class UserBox extends StatelessWidget {
         ],
       ),
       child: TextButton(
-          onPressed: () {},
-          child: Icon(
-            Icons.supervised_user_circle,
-            color: Colors.black,
-          ),
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(CircleBorder()),
-            enableFeedback: true,
-          )),
+        onPressed: () {},
+        child: Icon(
+          Icons.supervised_user_circle,
+          color: Colors.green,
+        ),
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(CircleBorder()),
+          enableFeedback: true,
+        ),
+      ),
     );
   }
 }
@@ -60,7 +62,7 @@ class LogoBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool keepwide = MediaQuery.of(context).size.width >= 900;
+    bool keepwide = MediaQuery.of(context).size.width >= 560;
 
     RichText logofull = RichText(
       textAlign: TextAlign.center,
@@ -70,6 +72,7 @@ class LogoBox extends StatelessWidget {
             fontSize: 36,
             fontWeight: FontWeight.w900,
           ),
+          textBaseline: TextBaseline.alphabetic,
         ),
         children: [
           TextSpan(
@@ -102,7 +105,7 @@ class LogoBox extends StatelessWidget {
 
     return Container(
       height: 50,
-      width: keepwide ? 300 : 60,
+      width: keepwide ? 280 : 70,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(60),
@@ -124,22 +127,81 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool keepwide = MediaQuery.of(context).size.width >= 600;
+    bool keepwide = MediaQuery.of(context).size.width >= 720;
 
-    return Container(
-      width: keepwide ? 400 : 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(40),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, -2),
-            blurRadius: 20,
-            color: Colors.black.withOpacity(0.7),
-          )
-        ],
-      ),
+    return Row(
+      children: [
+        UserBox(),
+        SizedBox(width: 15),
+        Container(
+          width: keepwide ? 110 : 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, -2),
+                blurRadius: 20,
+                color: Colors.black.withOpacity(0.7),
+              )
+            ],
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: keepwide
+                ? Text(
+                    "Regions",
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                        color: Colors.green,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  )
+                : Icon(
+                    Mdi.mapMarkerRadius,
+                    color: Colors.green,
+                  ),
+          ),
+        ),
+        SizedBox(width: 15),
+        Container(
+          width: keepwide ? 130 : 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(40),
+            border: Border.all(color: Colors.white, width: 3.5),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, -2),
+                blurRadius: 20,
+                color: Colors.black.withOpacity(0.7),
+              )
+            ],
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: keepwide
+                ? Text(
+                    "Geo Gallery",
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  )
+                : Icon(
+                    Mdi.earth,
+                    color: Colors.white,
+                  ),
+          ),
+        )
+      ],
     );
   }
 }
