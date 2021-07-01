c5407a8cb2fa98c5f6a10c4e4fa5a2fbc5a3eff5
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-02 03:28:55 +0530

    Create geogallerypage.dart
    - Added a file geogallerypage.dart inside the geogallery directory. This directory will contain all functionality related to the Geo Gallery feature
    - Added a widget GeoGalleryPage that acts as the root widget for the Geo Gallery

---
+++ lib/geogallery/geogallerypage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GeoGalleryPage extends StatelessWidget {
+  const GeoGalleryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
+    );
+  }
+}
