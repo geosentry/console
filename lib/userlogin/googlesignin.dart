7a4ff0a440ee94775a8c99fdf99740d5098f7db2
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-03 00:32:51 +0530

    Create googlesignin.dart
    - Added a file googlesignin.dart to implement the Google Sign flow with FIrebase Auth.
    - Implements a class GoogleSignInProvider that extends the ChangeNotifier class
    - Implements methods to login and logout

---
+++ lib/userlogin/googlesignin.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInProvider extends ChangeNotifier {
+  final googlesignin = GoogleSignIn();
+
+  GoogleSignInAccount? _user;
+
+  GoogleSignInAccount get user => _user!;
+
+  Future googlelogin() async {
+    final googleuser = await googlesignin.signIn();
+    if (googleuser == null) return;
+    _user = googleuser;
+
+    final googleauth = await googleuser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleauth.accessToken,
+      idToken: googleauth.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    notifyListeners();
+  }
+
+  Future logout() async {
+    await googlesignin.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
+}
