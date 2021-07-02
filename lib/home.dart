c3766aa17282e5b4ef7366691c0e47ce5b66a775
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-03 00:37:50 +0530

    Update Home with StreamBuilder
    - Updated the Home widget to use a StreamBuilder that listens on FirebaseAuth changes and repaints the application when auth status changes
    - Passes the Auth snapshot into the UserPage constructor

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,37 +15,42 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return SafeArea(
-      child: Container(
-        height: size.height,
-        width: size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("coast.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            CustomBar(),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(50),
-                child: Consumer<MenuState>(
-                  builder: (context, value, child) {
-                    if (value.currentmenu == MenuType.geogallery)
-                      return GeoGalleryPage();
-                    else if (value.currentmenu == MenuType.regions)
-                      return RegionsPage();
-                    else
-                      return UserPage();
-                  },
-                ),
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        return SafeArea(
+          child: Container(
+            height: size.height,
+            width: size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("islands.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
-        ),
-      ),
+            child: Column(
+              children: [
+                CustomBar(),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(50),
+                    child: Consumer<MenuState>(
+                      builder: (context, value, child) {
+                        if (value.currentmenu == MenuType.geogallery)
+                          return GeoGalleryPage();
+                        else if (value.currentmenu == MenuType.regions)
+                          return RegionsPage();
+                        else
+                          return UserPage(snapshot);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
