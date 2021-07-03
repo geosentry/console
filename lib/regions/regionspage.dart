16250354794dea24186711088070423872b7287d
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-04 01:53:28 +0530

    Update regionspage.dart
    - Added widget repsonsiveness based on screen size to move to top down layout when the screen is too narrow.
    - Added a widget RegionInfo that represents the block of data (currently a simple container)

--- lib/regions/regionspage.dart
+++ lib/regions/regionspage.dart
@@ -1,39 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:geosentry_console/regions/regionmenu.dart';
 
 class RegionsPage extends StatelessWidget {
   const RegionsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    if (size.width > 900)
+      return Container(
+        child: Row(
+          children: [
+            Flexible(
+              child: RegionInfo(),
+            ),
+            SizedBox(width: 0),
+            RegionMenu(),
+          ],
+        ),
+      );
+    else
+      return Container(
+        child: Column(
+          children: [
+            RegionMenu(),
+            SizedBox(height: 40),
+            Flexible(
+              child: RegionInfo(),
+            )
+          ],
+        ),
+      );
+  }
+}
+
+class RegionInfo extends StatelessWidget {
+  const RegionInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: [
-          Container(
-            width: 150,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-            ),
-          ),
-          SizedBox(width: 50),
-          Expanded(
-            flex: 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50),
-              ),
-            ),
-          ),
-          SizedBox(width: 50),
-          Container(
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-            ),
-          ),
-        ],
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
       ),
     );
   }
